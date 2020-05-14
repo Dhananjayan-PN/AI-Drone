21f017e4effb0148486e770fe6ec7ec5639dfd3b
Dhananjayan P N <pndjay@gmail.com>
2020-05-14 23:35:42 +0530

    Add files via upload

---
+++ Tello_Connect.py
@@ -0,0 +1,28 @@
+"""TELLO CONNECTOR"""
+from djitellopy import Tello
+import cv2
+import time
+
+tello = Tello()
+
+tello.connect()
+
+tello.takeoff()
+time.sleep(5)
+
+tello.move_up(20)
+time.sleep(5)
+
+tello.move_forward(300)
+time.sleep(5)
+
+"""tello.move_right(50)
+time.sleep(5)"""
+
+'''tello.rotate_clockwise(200)
+time.sleep(5)'''
+
+tello.land()
+time.sleep(1)
+        
+tello.end()
