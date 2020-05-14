a21263b19017c872cc07872e14843bf748d48003
Dhananjayan P N <pndjay@gmail.com>
2020-05-15 03:25:10 +0530

    changes

--- tello_face_recognition.py
+++ tello_face_recognition.py
@@ -19,12 +19,12 @@ try:
         if (_time - time.time() >= 5):
             print('{}%'.format(tello.get_battery))
             _time = time.time()
-        img = frame_read.frame
+        frame = frame_read.frame
         face_cascade = cv2.CascadeClassifier("frontalface.xml")
-        faces = face_cascade.detectMultiScale(img)
+        faces = face_cascade.detectMultiScale(frame)
         for x, y, w, h in faces:
             frame = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        cv2.imshow("drone", img)
+        cv2.imshow("drone", frame)
         key = cv2.waitKey(1)
         if key == ord('q'):
             tello.end()
