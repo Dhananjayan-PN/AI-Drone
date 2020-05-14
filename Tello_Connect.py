9104ff0b4438b0f14d3fc0be5d9a173a6466d817
Dhananjayan P N <pndjay@gmail.com>
2020-05-15 00:50:45 +0530

    tello connect edit

--- Tello_Connect.py
+++ Tello_Connect.py
@@ -13,15 +13,15 @@ tello.move_up(20)
 time.sleep(5)
 
 tello.move_forward(300)
-time.sleep(5)'''
+time.sleep(5)
 
-'''tello.move_right(50)
-time.sleep(5)'''
+tello.move_right(50)
+time.sleep(5)
 
-'''tello.rotate_clockwise(200)
-time.sleep(5)'''
+tello.rotate_clockwise(200)
+time.sleep(5)
 
-'''tello.land()
+tello.land()
 time.sleep(1)'''
 
 tello.end()
